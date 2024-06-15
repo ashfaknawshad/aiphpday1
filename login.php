84f1f463df6e566be193147780f5e18dfb634a7a
Ashfak Nawshad <ashfaknawshad@gmail.com>
2024-06-15 18:46:08 +0530

    modified:   dashboard.php renamed:    index.html -> index.php modified:   login.php modified:   register.php

--- login.php
+++ login.php
@@ -16,14 +16,14 @@
 <body style="background-color: #D3F9FF;">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="index.html"><img src="https://i.ibb.co/x74SzZ2/techwave-favicon.png"> TechWave</a>
+          <a class="navbar-brand" href="index.php"><img src="https://i.ibb.co/x74SzZ2/techwave-favicon.png"> TechWave</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 mr-auto">
               <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="index.html">Home</a>
+                <a class="nav-link " aria-current="page" href="index.php">Home</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="register.php">Register</a>
